263665ca0da6ffd423bf144c57a18aa95581e9a7
JulioViche <jvichecastillo@gmail.com>
2026-04-27 08:48:41 -0500

    chore: Inicializar el backend

---
+++ backend/main.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+app.use(express.json())
+
+mongoose
+  .connect(process.env.MONGsO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error(err))
+
+app.get('/', (req, res) => {
+  res.send('API funcionando')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`))
